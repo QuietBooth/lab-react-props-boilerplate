d7178dc1fcabab2fee4c5ee85c97ee840180f3c2
Nivaash <nidishnivash@gmail.com>
2023-12-17 19:19:07 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import elephant from "./images/elephant.jpeg";
+import React from 'react';
+
+
+function App(props) {
+  let image_arr = props.imageData()
+  // code here
+  return(
+    <>
+      <h1 className='heading'>Kalvium Gallery</h1>
+      <div className='images'>
+        
+        <img src={elephant} className='img' alt="" />
+        <img src={elephant} className='img' alt="" />
+        <img src={elephant} className='img' alt="" />
+        <img src={elephant} className='img' alt="" />
+      </div>
+    </>
+  )
+}
+
+export default App;
